3de276d618937420568711714bccae3364ae7f0e
Fad OURO AGORO <fadouro@gmail.com>
2023-12-25 21:44:48 +0400

    test(Employee): Add tests for employee feature

---
+++ LeaveManagement.API.IntegrationTests/Support/Utilities.cs
@@ -0,0 +1,33 @@
+﻿using LeaveManagement.Domain.Entities;
+using LeaveManagement.Persistence.Data;
+
+namespace LeaveManagement.API.IntegrationTests.Support;
+
+internal static class Utilities
+{
+    public static void InitializeEmployeesForTests(ApplicationContext context)
+    {
+        List<Employee>? initialEmployees =
+        [
+            new() 
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "john.doe@example.com",
+                PhoneNumber = "123456789"
+            },
+            new() 
+            {
+                Id = 2,
+                FirstName = "Jane",
+                LastName = "Smith",
+                Email = "jane.smith@example.com",
+                PhoneNumber = "987654321"
+            }
+        ];
+
+        context.Employees.AddRange(initialEmployees);
+        context.SaveChanges();
+    }
+}
