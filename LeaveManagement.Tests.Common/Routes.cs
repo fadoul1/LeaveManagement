364e3894178bb62ea797f485216a1722b483da20
Fad OURO AGORO <fadouro@gmail.com>
2023-12-25 21:44:48 +0400

    test(Employee): Add tests for employee feature

---
+++ LeaveManagement.Tests.Common/Routes.cs
@@ -0,0 +1,7 @@
+﻿namespace LeaveManagement.Tests.Common;
+
+public static class Routes
+{
+    public const string EmployeeRessource = "/api/employee";
+    public const string LeaveRessource = "/api/leaves";
+}
