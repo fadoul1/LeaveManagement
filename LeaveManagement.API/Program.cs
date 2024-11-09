1e03fc36b079a8d53bcc76d0283235bcd5e2b1e0
Fad OURO AGORO <fadouro@gmail.com>
2024-11-10 02:09:40 +0400

    feat: update aspire and replace specflow by reqnrolll for BDD tests

--- LeaveManagement.API/Program.cs
+++ LeaveManagement.API/Program.cs
@@ -2,16 +2,12 @@ using LeaveManagement.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.AddServiceDefaults();
-
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder
-       .ConfigureServices()
-       .ConfigurePipeline();
+var app = builder.ConfigureServices().ConfigurePipeline();
 
 await app.CreateDatabaseAsync();
 
--- LeaveManagement.AppHost/Program.cs
+++ LeaveManagement.AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.LeaveManagement_API>("leavemanagement.api");
+builder.AddProject<Projects.LeaveManagement_API>("leavemanagement-api");
 
 builder.Build().Run();
