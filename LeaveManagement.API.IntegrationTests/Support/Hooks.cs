afa0cb36a78625a2f0787550c76d91f284db0c23
fadoul1 <fadouro@gmail.com>
2025-12-17 16:24:02 +0400

    test: implement Leave API integration tests

---
+++ LeaveManagement.API.IntegrationTests/Support/Hooks.cs
@@ -0,0 +1,29 @@
+﻿using Reqnroll.BoDi;
+
+namespace LeaveManagement.API.IntegrationTests.Support;
+
+[Reqnroll.Binding]
+internal class Hooks
+{
+    private static CustomWebApplicationFactory<Program>? _factory;
+
+    [Reqnroll.BeforeTestRun]
+    public static async Task BeforeTestRun()
+    {
+        _factory = new CustomWebApplicationFactory<Program>();
+        await _factory.InitializeAsync();
+    }
+
+    [Reqnroll.AfterTestRun]
+    public static async Task AfterTestRun()
+    {
+        await _factory.DisposeAsync();
+    }
+
+    [Reqnroll.BeforeScenario(Order = 0)]
+    public void RegisterFactory(IObjectContainer objectContainer)
+    {
+        objectContainer.RegisterInstanceAs(_factory);
+    }
+}
+
