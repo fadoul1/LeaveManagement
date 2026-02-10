5c11b94ee8b54fa16b169cdd7ad1a30601d16352
fadoul1 <fadouro@gmail.com>
2026-02-10 16:00:54 +0400

    folder restructure

--- LeaveManagement.Application/Features/Employees/Queries/GetemployeeById/GetEmployeeByIdQuery.cs
+++
@@ -1,9 +0,0 @@
-﻿using LeaveManagement.Application.Responses;
-using MediatR;
-
-namespace LeaveManagement.Application.Features.Employees.Queries.GetEmployeeById;
-
-public class GetEmployeeByIdQuery : IRequest<EmployeeResponse>
-{
-    public long EmployeeId { get; set; }
-}
--- LeaveManagement.Tests.Common/Routes.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace LeaveManagement.Tests.Common;
-
-public static class Routes
-{
-    public const string EmployeeRessource = "/api/employees";
-    public const string LeaveRessource = "/api/leaves";
-}
